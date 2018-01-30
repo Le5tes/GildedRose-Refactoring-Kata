8759bac422ad5eef9268b8cc771b20c10aeae6cc
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-30 11:36:01 +0000

    further test for regular items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -2,7 +2,7 @@ describe("Gilded Rose", function() {
 
   describe("regular items", function(){
     beforeEach(function(){
-  	  const gildedRose = new Shop([ new Item("foo", 10, 10) ]);
+  	  const gildedRose = new Shop([ new Item("foo", 10, 10), new Item("bar", 0, 10), new Item("baz", 10, 0) ]);
   	  items = gildedRose.updateQuality();
     })
  
@@ -16,6 +16,14 @@ describe("Gilded Rose", function() {
 
     it("should reduce the sellIn value by one", function(){
       expect(items[0].sellIn).toEqual(9);
+    });
+
+    it("should degrade by 2 once the sellIn value is 0",  function(){
+      expect(items[1].quality).toEqual(8);
+    });
+
+    it("should not degrade below 0", function(){
+      expect(items[2].quality).not.toEqual(-1);
     });
   });
   
