2744fe7297b1348146de7dd3a3e75f9faaecbf47
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-30 11:27:43 +0000

    some basic passing tests for regular items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,23 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  describe("regular items", function(){
+    beforeEach(function(){
+  	  const gildedRose = new Shop([ new Item("foo", 10, 10) ]);
+  	  items = gildedRose.updateQuality();
+    })
+ 
+    it("should keep the name", function() {      
+      expect(items[0].name).toEqual("foo");
+    });
+
+    it("should degrade by one when they are still in date",function(){
+  	  expect(items[0].quality).toEqual(9);
+    });
+
+    it("should reduce the sellIn value by one", function(){
+      expect(items[0].sellIn).toEqual(9);
+    });
   });
+  
 
 });
