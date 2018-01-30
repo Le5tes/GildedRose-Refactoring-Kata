2044273a5e91422e35ea499e3e4d22fdb9dd14da
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-30 13:22:46 +0000

    replaced main if statement with switch statement

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -15,31 +15,34 @@ class Shop {
       if (this._isNotSulphuras(i)){
         this.items[i].sellIn = this.items[i].sellIn - 1;
 
-        if (this.items[i].name == 'Aged Brie'){
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          };
+        switch (this.items[i].name) {
+          case 'Aged Brie':
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
+            };
+            break;
 
-        } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].sellIn < 0) {
-            this.items[i].quality = 0
-          } else if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-            if (this.items[i].sellIn < 11) {
-              this.items[i].quality = this.items[i].quality + 1;
-            };
-            if (this.items[i].sellIn < 6) {
-              this.items[i].quality = this.items[i].quality + 1;
-            };
-          }
-          
-        } else {
-          if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
+          case 'Backstage passes to a TAFKAL80ETC concert': 
             if (this.items[i].sellIn < 0) {
-              this.items[i].quality = this.items[i].quality - 1;
+              this.items[i].quality = 0
+            } else if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
+              if (this.items[i].sellIn < 11) {
+                this.items[i].quality = this.items[i].quality + 1;
+              };
+              if (this.items[i].sellIn < 6) {
+                this.items[i].quality = this.items[i].quality + 1;
+              };
             }
-          } 
+            break;
+
+          default:
+            if (this.items[i].quality > 0) {
+              this.items[i].quality = this.items[i].quality - 1;
+              if (this.items[i].sellIn < 0) {
+                this.items[i].quality = this.items[i].quality - 1;
+              }
+            } 
         };
       }
     }
