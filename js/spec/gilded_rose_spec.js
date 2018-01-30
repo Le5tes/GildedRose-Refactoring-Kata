2ca99672de07bb8676f8d44bb3da3be39d2fee09
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-30 11:51:12 +0000

    adds tests for 'Sulfuras' functionality

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -40,7 +40,22 @@ describe("Gilded Rose", function() {
   	it("should never increase in quality above 50", function(){
   	  expect(items[1].quality).not.toEqual(51);
   	});
-  })
+  });
+
+  describe("Sulfuras",function(){
+  	beforeEach(function(){
+  	  const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+  	  items = gildedRose.updateQuality();
+    });
+
+    it("should never change in quality", function(){
+      expect(items[0].quality).toEqual(80);
+    });
+
+    it("should not change it's sellIn", function(){
+      expect(items[0].sellIn).toEqual(0);
+    });
+  });
   
 
 });
