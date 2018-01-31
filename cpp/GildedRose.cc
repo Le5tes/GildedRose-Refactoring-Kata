ef4d102e977f390afa458ae653d9b8ad12edb061
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-31 16:30:36 +0000

    refactored sulfuras to outside

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -7,65 +7,13 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (items[i].quality > 0)
-            {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
+            {   
+            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
+                if (items[i].quality > 0)
+                {                    
+                    items[i].quality = items[i].quality - 1;                   
                 }
             }
             else
@@ -73,6 +21,52 @@ void GildedRose::updateQuality()
                 if (items[i].quality < 50)
                 {
                     items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (items[i].sellIn < 11)
+                        {
+                            if (items[i].quality < 50)
+                            {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6)
+                        {
+                            if (items[i].quality < 50)
+                            {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+                items[i].sellIn = items[i].sellIn - 1;
+
+            if (items[i].sellIn < 0)
+            {
+                if (items[i].name != "Aged Brie")
+                {
+                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (items[i].quality > 0)
+                        {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                    else
+                    {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                }
+                else
+                {
+                    if (items[i].quality < 50)
+                    {
+                        items[i].quality = items[i].quality + 1;
+                    }
                 }
             }
         }
