e98ddf21f8694b33754a1860511a6b84fbdc46c6
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-31 18:06:57 +0000

    refactored logic into the three trees for the types of items

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -9,21 +9,26 @@ void GildedRose::updateQuality()
     {
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
             {   
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (items[i].quality > 0)
-                {                    
-                    items[i].quality = items[i].quality - 1;                   
-                }
-            }
-            else
-            {
+            items[i].sellIn = items[i].sellIn - 1;
+            if(items[i].name == "Aged Brie")
+            {     
                 if (items[i].quality < 50)
                 {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                }
+                break;
+            }
+            else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (items[i].quality < 50)
+                {   
+                    if (items[i].sellIn <0 )
                     {
+                        items[i].quality = 0;
+                    }
+                    else 
+                    {
+                        items[i].quality = items[i].quality + 1;
                         if (items[i].sellIn < 11)
                         {
                             if (items[i].quality < 50)
@@ -42,33 +47,19 @@ void GildedRose::updateQuality()
                     }
                 }
             }
-
-                items[i].sellIn = items[i].sellIn - 1;
-
-            if (items[i].sellIn < 0)
+            else
             {
-                if (items[i].name != "Aged Brie")
-                {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].quality > 0)
+                {                 
+                    items[i].quality = items[i].quality - 1;                   
+                    if (items[i].sellIn < 0)
                     {
-                        if (items[i].quality > 0)
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
-                else
-                {
-                    if (items[i].quality < 50)
-                    {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+
             }
+
         }
     }
 }
